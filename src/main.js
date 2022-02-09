a7463da39f4ea5cbeb8194cd9d09db969978752d
acaiibawl <hagiya0719@gmail.com>
2022-02-09 11:35:12 +0900

    VueRouterで画面の切り替え

--- src/main.js
+++ src/main.js
@@ -1,8 +1,37 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter);
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/main',
+      component: () => import('./components/MainPage.vue')
+    },
+    {
+      path: '/study/binding',
+      component: () => import('./components/study/BindingPage.vue')
+    },
+    {
+      path: '/study/directive',
+      component: () => import('./components/study/DirectivePage.vue')
+    },
+    {
+      path: '/study/event',
+      component: () => import('./components/study/EventPage.vue')
+    },
+    {
+      path: '/*',
+      redirect: '/main',
+    }
+  ]
+});
+
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
