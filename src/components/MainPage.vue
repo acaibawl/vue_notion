96b01dae2d96f1889d213e2e1024b5271b3ac566
acaiibawl <hagiya0719@gmail.com>
2022-02-09 13:43:11 +0900

    ノート一覧のマウスオーバー時の見た目調整

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -31,6 +31,7 @@ export default {
             this.noteList.push({
                 id: new Date().getTime().toString(16),
                 name: "新規ノート",
+                mouseover: false,
             });
         },
     },
